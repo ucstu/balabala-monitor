70faf84ff5c522e18428695f2a5488bae59317ad
董健 <1174115923@qq.com>
2022-07-29 00:06:08 +0800

    换行符问题

--- packages/juejin/backend/src/app.module.ts
+++ packages/juejin/backend/src/app.module.ts
@@ -2,23 +2,26 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ArticleModule } from './article/article.module';
+import { ArticleModule } from "./article/article.module";
 import { Article } from "./entity/article.entity";
-import { UserModule } from './user/user.module';
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [UserModule, ArticleModule,TypeOrmModule.forRoot(
-          {
-            "type": "mysql",
-            "host": "***REMOVED***8",
-            "port": 3306,
-            "username":"***REMOVED***",
-            "password": "***REMOVED***",
-            "database":"***REMOVED***",
-            "synchronize": false,  // 自动载入的模型将同步
-            'autoLoadEntities': true,// 自动加载实体类
-        }
-        ),TypeOrmModule.forFeature([Article])],
+  imports: [
+    UserModule,
+    ArticleModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "***REMOVED***8",
+      port: 3306,
+      username: "***REMOVED***",
+      password: "***REMOVED***",
+      database: "***REMOVED***",
+      synchronize: false, // 自动载入的模型将同步
+      autoLoadEntities: true, // 自动加载实体类
+    }),
+    TypeOrmModule.forFeature([Article]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/juejin/backend/src/entity/user.entity.ts
+++ packages/juejin/backend/src/entity/user.entity.ts
@@ -2,24 +2,24 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity("content")
 export class Account {
-    /**
-     * 账号ID
-     */
-    @PrimaryGeneratedColumn({name:"id"})
-    id: number;
-    /**
-     * 昵称
-     */
-    @Column({name:"name"})
-    name: string;
-    /**
-     * 用户名称
-     */
-    @Column({name:"password"})
-    password: string;
-    /**
-     * 电话号码
-     */
-    @Column({name:"phone"})
-    phone: string;
+  /**
+   * 账号ID
+   */
+  @PrimaryGeneratedColumn({ name: "id" })
+  id: number;
+  /**
+   * 昵称
+   */
+  @Column({ name: "name" })
+  name: string;
+  /**
+   * 用户名称
+   */
+  @Column({ name: "password" })
+  password: string;
+  /**
+   * 电话号码
+   */
+  @Column({ name: "phone" })
+  phone: string;
 }

2e92df2766c38bb010cf66a1315b376cfe041ccf
董健 <1174115923@qq.com>
2022-07-29 00:06:08 +0800

    换行符问题

--- packages/juejin/backend/src/app.module.ts
+++ packages/juejin/backend/src/app.module.ts
@@ -2,23 +2,26 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ArticleModule } from './article/article.module';
+import { ArticleModule } from "./article/article.module";
 import { Article } from "./entity/article.entity";
-import { UserModule } from './user/user.module';
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [UserModule, ArticleModule,TypeOrmModule.forRoot(
-          {
-            "type": "mysql",
-            "host": "139.155.90.228",
-            "port": 3306,
-            "username":"jkpt",
-            "password": "123456",
-            "database":"juejin_ht",
-            "synchronize": false,  // 自动载入的模型将同步
-            'autoLoadEntities': true,// 自动加载实体类
-        }
-        ),TypeOrmModule.forFeature([Article])],
+  imports: [
+    UserModule,
+    ArticleModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "139.155.90.228",
+      port: 3306,
+      username: "jkpt",
+      password: "123456",
+      database: "juejin_ht",
+      synchronize: false, // 自动载入的模型将同步
+      autoLoadEntities: true, // 自动加载实体类
+    }),
+    TypeOrmModule.forFeature([Article]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/juejin/backend/src/article/article.controller.ts
+++ packages/juejin/backend/src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller } from "@nestjs/common";
 
-@Controller('article')
+@Controller("article")
 export class ArticleController {}

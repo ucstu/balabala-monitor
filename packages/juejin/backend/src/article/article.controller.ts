87775cb53123254e2da306a83a2f51b1a6dd60e7
董健 <1174115923@qq.com>
2022-07-29 13:18:07 +0800

    新增文章接口完成，差jwt的token需要phone

--- packages/juejin/backend/src/article/article.controller.ts
+++ packages/juejin/backend/src/article/article.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, Headers } from "@nestjs/common";
+import { Article } from "src/entity/article.entity";
+import { ArticleService } from "./article.service";
 
 @Controller("article")
-export class ArticleController {}
+export class ArticleController {
+  constructor(private articleService: ArticleService) {}
+
+  @Post()
+  insert(@Body() article: Article, @Headers() Headers) {
+    const cust = this.articleService.insert(article, Headers.token);
+    return cust;
+  }
+}

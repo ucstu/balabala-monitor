e84978baec671912952677c78d6db5a62354ac4e
董健 <1174115923@qq.com>
2022-07-29 13:18:07 +0800

    新增文章接口完成，差jwt的token需要phone

--- packages/juejin/backend/src/article/article.module.ts
+++ packages/juejin/backend/src/article/article.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Article } from "src/entity/article.entity";
 import { ArticleController } from "./article.controller";
 import { ArticleService } from "./article.service";
 
 @Module({
   controllers: [ArticleController],
   providers: [ArticleService],
+  imports: [TypeOrmModule.forFeature([Article])],
 })
 export class ArticleModule {}
--- packages/juejin/backend/src/article/article.service.ts
+++ packages/juejin/backend/src/article/article.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Article } from "src/entity/article.entity";
+import { responseRust } from "src/entity/responseRust";
+import { Repository } from "typeorm";
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  constructor(
+    @InjectRepository(Article)
+    private articleMapper: Repository<Article>
+  ) {}
+
+  //插入文章
+
+  async insert(article: Article, token: string) {
+    const rust = new responseRust();
+
+    //通过token获得电话号码；
+    article.phone = token;
+    //插入
+    const r = await this.articleMapper.insert(article);
+
+    if (r.raw.affectedRows > 0) {
+      //插入成功
+      article.articleId = r.raw.insertId;
+      return rust.success_data(article);
+    } else {
+      // 插入失败
+      return rust.error();
+    }
+  }
+
+  //修改文章
+
+  //删除文章
+
+  //查询所有文章 （可能会有类别）
+}

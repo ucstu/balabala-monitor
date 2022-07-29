e1ac4c6424299432d8d05b90c03f2c0f2d550a21
leben <1979044994@qq.com>
2022-07-29 15:07:52 +0000

    feat(路由): 添加路由信息
    添加部分基础路由信息

---
+++ packages/juejin/frontend/src/pages/HomePage/ChildPage/Frontend.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+Frontend
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- packages/juejin/frontend/src/vite-env.d.ts
+++ packages/juejin/frontend/src/vite-env.d.ts
@@ -1,11 +1,7 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<
-    Record<string, unknown>,
-    Record<string, unknown>,
-    any
-  >;
-  export default component;
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }

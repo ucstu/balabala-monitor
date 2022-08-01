b44cc2a48fbeabc2a8283350d81782e5d2c2f6a7
董健 <1174115923@qq.com>
2022-08-01 21:43:14 +0800

    基础指标接口正在开发，今天事情有点多，写的代码不多

--- packages/monitor/backend/src/module/basicindicator/basicindicator.controller.ts
+++ packages/monitor/backend/src/module/basicindicator/basicindicator.controller.ts
@@ -1,14 +1,17 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { BasicIndicator } from "src/entity/basicIndicator.entity";
+import { responseRust } from "src/entity/responseRust";
 import { BasicindicatorService } from "./basicindicator.service";
 
 /**
- * 基础基表controllera
+ * 基础指标controllera
  */
 @Controller("/performances")
 export class BasicindicatorController {
   constructor(private basicindicator: BasicindicatorService) {}
 
   @Post("/basicindicators")
-  private basicindicatorLoad(@Body() basicIndicator: BasicIndicator) {}
+  private basicindicatorLoad(@Body() basicIndicator: BasicIndicator) {
+    return this.basicindicator.upLoadBasicindicator(basicIndicator);
+  }
 }

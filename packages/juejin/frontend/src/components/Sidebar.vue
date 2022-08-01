8a70b35e9c37862155dec6478c04ff7c27f0b776
Z--Yifan <1279640758@qq.com>
2022-08-01 15:16:23 +0800

    增加侧边栏组件

---
+++ packages/juejin/frontend/src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="author">
+      <span>优质作者</span>
+      <div v-for="item in authorName" :key="item">
+        <img src="" alt="" />
+        {{ item }}
+      </div>
+    </div>
+    <div>签到</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const authorName: Array<string> = ["aaa", "bbbb", "cc"];
+</script>
+
+<style lang="scss" scoped>
+.author {
+  color: rgb(15, 52, 137);
+}
+</style>

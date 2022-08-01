9d9feb3ce4ad365eaf923e65bb287d5fc62d6858
Z--Yifan <1279640758@qq.com>
2022-08-01 15:18:43 +0800

    32

--- packages/juejin/frontend/src/components/Divider.vue
+++ packages/juejin/frontend/src/components/Divider.vue
@@ -1,29 +1,28 @@
 <template>
-    <span class="divider" :style=props ></span>
+  <span class="divider" :style="props"></span>
 </template>
 
 <script lang="ts" setup>
-const props=defineProps({
-    color:{
-        type:String,
-        default:'#ccc'
-    },
-   display: {
+const props = defineProps({
+  color: {
     type: String,
-    default: 'block'
-    }
-})
+    default: "#ccc",
+  },
+  display: {
+    type: String,
+    default: "block",
+  },
+});
 console.log(props);
-
 </script>
 
 <style lang="scss" scoped>
 .divider {
-  color: rgb(0, 0, 0,0.1);
+  color: rgb(0, 0, 0, 0.1);
   display: inline-block;
   height: 1px;
   width: 100%;
-  opacity: .6;
-  background: rgb(0, 0, 0,0.1);
+  opacity: 0.6;
+  background: rgb(0, 0, 0, 0.1);
 }
 </style>

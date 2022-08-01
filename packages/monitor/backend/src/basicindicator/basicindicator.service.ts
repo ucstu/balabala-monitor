68b572b5ab1397c8f06dac05a7039d911bb71675
董健 <1174115923@qq.com>
2022-08-01 14:13:43 +0800

    监控平台开始工作

--- packages/monitor/backend/src/basicindicator/basicindicator.service.ts
+++ packages/monitor/backend/src/basicindicator/basicindicator.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { ElasticsearchService } from "@nestjs/elasticsearch";
+import { BasicIndicator } from "src/entity/basicIndicator.entity";
 
 @Injectable()
-export class BasicindicatorService {}
+export class BasicindicatorService {
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
+  private basicindicatorLoad(basicIndicator: BasicIndicator) {}
+}

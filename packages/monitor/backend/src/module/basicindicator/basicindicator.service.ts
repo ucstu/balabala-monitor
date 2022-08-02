457436fc7ada52e7b8591167b78657a625927285
董健 <1174115923@qq.com>
2022-08-02 15:44:01 +0800

    一点小更新

--- packages/monitor/backend/src/module/basicindicator/basicindicator.service.ts
+++ packages/monitor/backend/src/module/basicindicator/basicindicator.service.ts
@@ -3,6 +3,7 @@ import { ElasticsearchService } from "@nestjs/elasticsearch";
 import { basicbehaviorIndex } from "src/config/db.index";
 import { BasicIndicator } from "src/entity/basicIndicator.entity";
 import { responseRust } from "src/entity/responseRust";
+import { BasicindicatorsVo } from "src/vo/basicindicators.vo";
 
 @Injectable()
 export class BasicindicatorService {
@@ -30,5 +31,57 @@ export class BasicindicatorService {
   /**
    * 查询数据
    *
+   */
+  async queryBasicindicator(querys: BasicindicatorsVo) {
+    const res = await this.elasticsearchService.search({
+      index: basicbehaviorIndex,
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                term: {
+                  appId: querys.appid,
+                },
+              },
+              {
+                term: {
+                  userid: querys.userid,
+                },
+              },
+              {
+                term: {
+                  pageurl: querys.pageurl,
+                },
+              },
+              {
+                term: {
+                  type: querys.type,
+                },
+              },
+              {
+                term: {
+                  subType: querys.subType,
+                },
+              },
+              {
+                range: {
+                  startTime: {
+                    gte: new Date(querys.starttime).getTime(),
+                    lte: new Date(querys.endtime).getTime(),
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    return res;
+  }
+
+  /**
+   *统计数据
    */
 }

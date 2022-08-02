d03354b32f0f5ca656585cca1057fd5cefabbdc0
张腾飞 <2936408477@qq.com>
2022-08-02 15:43:03 +0800

    feat: 完成资源错误模块

--- packages/monitor/backend/src/module/resourceerror/resourceerror.controller.ts
+++ packages/monitor/backend/src/module/resourceerror/resourceerror.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, HttpCode, Post, Query } from "@nestjs/common";
 import { ResourceError } from "src/entity/resourceError.entity";
 import { responseRust } from "src/entity/responseRust";
-import { ResourceerrorTotalVo } from "src/vo/resourceerror.vo";
+import { ResourceerrorTotalVo, ResourceerrorVo } from "src/vo/resourceerror.vo";
 import { ResourceerrorService } from "./resourceerror.service";
 
 @Controller("/errors")
@@ -15,5 +15,9 @@ export class ResourceerrorController {
   @Get("/resourceerrorstatistics")
   totalError(@Query() resourceerrorTotalVo: ResourceerrorTotalVo) {
     return this.resourceerrorService.totalError(resourceerrorTotalVo);
+  }
+  @Get("/resourceerrors")
+  getErrorList(@Query() resourceerrorVo: ResourceerrorVo) {
+    return this.resourceerrorService.getErrorList(resourceerrorVo);
   }
 }

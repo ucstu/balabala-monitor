9a6af6f54e2b1cc612cce734d3034f4601cbf1bd
Z--Yifan <1279640758@qq.com>
2022-08-02 16:31:43 +0800

    daw

--- packages/juejin/frontend/src/components/Sidebar.vue
+++ packages/juejin/frontend/src/components/Sidebar.vue
@@ -1,21 +1,78 @@
 <template>
-  <div>
-    <div class="author">
-      <span>优质作者</span>
-      <div v-for="item in authorName" :key="item">
-        {{ item }}
+  <div class="box">
+    <!-- 左侧内容 -->
+    <div class="container"></div>
+    <!-- 签到 -->
+    <div class="signin">
+      <div class="sigin-top">
+        <img class="rili" src="../assets/rili.png" alt="" />
+        <span class="title">上午好!</span>
+        <a href="">
+          <img class="signin-jpg" src="../assets/signin.png" />
+        </a>
       </div>
+      <div class="signin2">点亮你在社区的每一天</div>
     </div>
-    <div>签到</div>
+    <div class="guanggao-img">
+      <img src="../assets/guanggao.png" alt="" />
+      <img src="../assets/guangao2.png" alt="" />
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-const authorName: Array<string> = ["aaa", "bbbb", "cc"];
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-.author {
-  color: rgb(15, 52, 137);
+.box {
+  width: 1200px;
+  height: 2000px;
+  margin-top: 22px;
+  text-align: center;
+  .container {
+    width: 880px;
+    height: 2000px;
+    float: left;
+  }
+  .signin {
+    width: 240px;
+    height: 96px;
+    background-color: aqua;
+    // 为了看清才用的这个颜色
+    padding: 16px;
+    float: right;
+    box-sizing: border-box;
+    margin-bottom: 16px;
+    .sigin-top {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 8px;
+      .rili {
+        display: flex;
+      }
+      .title {
+        display: flex;
+        color: #1d2129;
+        font-size: 18px;
+        font-weight: 500;
+        margin-top: 2px;
+      }
+      .signin-jpg {
+        display: flex;
+        width: 72px;
+        height: 32px;
+      }
+    }
+  }
+  .signin2 {
+    margin-left: 36px;
+    color: #4e5969;
+    font-size: 14px;
+  }
+  .guanggao-img img {
+    width: 240px;
+    height: 200px;
+    float: right;
+    margin-bottom: 16px;
+  }
 }
 </style>

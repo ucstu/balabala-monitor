40ac48f1b91dee7a6432c2337f015c21b086eff9
张旭东 <2219454275@qq.com>
2022-08-03 17:38:23 +0000

    调整sdk结构，重新生成类型声明文件

--- packages/monitor/sdk/src/monitoring/error/initJavaScriptError.ts
+++ packages/monitor/sdk/src/monitoring/error/initJavaScriptError.ts
@@ -2,15 +2,15 @@ import { getBasicParams } from "src/common/utils";
 import { reportWithCache } from "src/reporting";
 
 export default function initJavaScriptError(): void {
-  window.onerror = (msg, line, column) => (e: { reason: { stack: any } }) => {
+  window.onerror = (msg, url, line, column, error) => {
     reportWithCache("JavaScriptError", {
       mainType: 2,
       subType: 2001,
-      msg: msg,
-      line: line,
-      column: column,
+      msg: typeof msg === "string" ? msg : JSON.stringify(msg),
+      line: line || -1,
+      column: column || -1,
       errorTime: performance.now(),
-      stack: e.reason?.stack,
+      stack: error?.stack || "no stack trace available",
       ...getBasicParams(),
     });
   };
--- packages/monitor/sdk/src/monitoring/error/initResourceError.ts
+++ packages/monitor/sdk/src/monitoring/error/initResourceError.ts
@@ -1,28 +1,25 @@
-import { getBasicParams } from "src/common/utils";
-import { reportWithCache } from "src/reporting";
-
 export default function initResourceError(): void {
   window.addEventListener(
     "error",
-    (e: EventTarget | any) => {
-      const target: EventTarget | any = e.target;
-      if (!target) return;
+    (e: ErrorEvent) => {
+      console.log(e);
+      // const target = e.target;
+      // if (!target) return;
 
-      if (target.src || target.href) {
-        const url = target.src || target.href;
-        reportWithCache("ResourceError", {
-          mainType: 1,
-          subType: 1001,
-          errorTime: e.timeStamp,
-          resourceType: target.tagName,
-          html: target.outerHTML,
-          path: e.path
-            .map((item: { tagName: any }) => item.tagName)
-            .filter(Boolean),
-          // pageUrl:url,
-          ...getBasicParams(),
-        });
-      }
+      // if (target. || target.href)
+      //   const url = target.src || target.href;
+      //   reportWithCache("ResourceError", {
+      //     mainType: 1,
+      //     subType: 1001,
+      //     errorTime: e.timeStamp,
+      //     resourceType: target.tagName,
+      //     html: target.outerHTML,
+      //     path: e.path
+      //       .map((item: { tagName: any }) => item.tagName)
+      //       .filter(Boolean),
+      //     ...getBasicParams(),
+      //   });
+      // }
     },
     true
   );

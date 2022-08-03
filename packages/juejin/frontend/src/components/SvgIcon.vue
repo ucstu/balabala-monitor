13df07285c378c11e54331d14f402378b5f5c4b0
leben <1979044994@qq.com>
2022-08-03 17:07:21 +0000

    编辑页面头部，引入svg

---
+++ packages/juejin/frontend/src/components/SvgIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg aria-hidden="true">
+    <use :xlink:href="symbolId" :fill="color" />
+  </svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    prefix?: string;
+    name: string;
+    color?: string;
+  }>(),
+  {
+    prefix: "icon",
+    color: "#333",
+  }
+);
+
+const symbolId = computed(() => `#${props.prefix}-${props.name}`);
+</script>

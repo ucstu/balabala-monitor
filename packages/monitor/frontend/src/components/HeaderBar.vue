af2e9d357780d89b8e976bb88825a9a47a56d86d
张旭东 <2219454275@qq.com>
2022-08-03 14:43:17 +0000

    完成监控前端项目的初始化，路由，页面结构等

---
+++ packages/monitor/frontend/src/components/HeaderBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <header style="background-color: blue"></header>
+</template>
+
+<script setup lang="ts">
+import type { RouteLocationNormalizedLoaded } from "vue-router";
+
+const props = defineProps<{
+  route: RouteLocationNormalizedLoaded;
+}>();
+</script>
+
+<style scoped lang="scss"></style>
---
+++ packages/monitor/frontend/src/pages/HomePage/HomePage.vue
@@ -0,0 +1,7 @@
+<template>
+  <main><span class="fa fa-vcard"></span></main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>

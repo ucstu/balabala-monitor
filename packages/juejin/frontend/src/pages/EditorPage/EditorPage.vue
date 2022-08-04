503a4b1b2a6da5faad73c4b020d16b55de46c40c
leben <1979044994@qq.com>
2022-08-04 13:55:45 +0000

    编辑页面样式完成，以及部分内容

--- packages/juejin/frontend/src/pages/EditorPage/EditorPage.vue
+++ packages/juejin/frontend/src/pages/EditorPage/EditorPage.vue
@@ -41,64 +41,129 @@
     <!--功能按钮区-->
     <div class="button_bar">
       <div class="button-left">
-        <SvgIcon
-          name="ziyuan"
-          style="width: 15px"
-          class="button-editor"
-          @click="addBold"
-        ></SvgIcon>
-        <svg class="icon" aria-hidden="true">
-          <use xlink:href="#icon-ziyuan"></use>
-        </svg>
-        <div>
+        <div class="button-editor" v-for="item in svgNames" :key="item">
           <SvgIcon
-            name="ziyuan"
-            style="width: 15px"
-            class="button-editor"
-            @click="addBold"
+            :name="item"
+            style="width: 18px"
+            @click="addinfo()"
           ></SvgIcon>
         </div>
-        <span @click="addUnderline"><B>B</B></span>
-        <span @click="addItalic"><B>I</B></span>
       </div>
       <div></div>
     </div>
 
     <!--主要内容区-->
     <div class="content_bar">
-      <!--markdown编辑器区-->
+      <!-- markdown编辑器区 -->
       <div class="markdown_body">
         <textarea
           ref="ref_md_edit"
           class="md_textarea_content"
+          style="font-size: 24px"
           v-model="markString"
         >
         </textarea>
       </div>
-
       <!--解析成html区-->
       <div class="html_body">
         <p v-html="htmlString"></p>
       </div>
     </div>
   </div>
+  <div class="bottom">
+    <div class="bytemd-status-left">
+      <span>字数: <strong>0</strong></span
+      ><span>行数: <strong>1</strong></span>
+    </div>
+    <div class="bytemd-status-right">
+      <label><input type="checkbox" />同步滚动</label><span>回到顶部</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import SvgIcon from "@/components/SvgIcon.vue";
-import { ref } from "vue";
-
-const addBold = () => {
-  console.log(111);
-};
-const addUnderline = () => {
-  console.log(222);
-};
-const addItalic = () => {
-  console.log(333);
-};
+import hljs from "highlight.js"; //对代码进行语法高亮的库
+import { marked } from "marked"; //解析mardown语法的库
+import { nextTick, Ref, ref, watch } from "vue";
 const markString = ref("");
 const htmlString = ref("");
+watch(
+  () => markString,
+  (newVal, oldVal) => {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+    });
+    htmlString.value = marked(oldVal);
+  }
+);
+watch(
+  () => htmlString,
+  (newVal, oldVal) => {
+    nextTick(() => {
+      const codes = document.querySelectorAll(".code");
+      codes.forEach((item: any) => {
+        item.innerHTML =
+          "<ul><li>" +
+          item.innerHTML.replace(/\n/g, "\n</li><li>") +
+          "\n</li></ul>";
+        hljs.highlightBlock(item);
+      });
+    });
+  }
+);
+const addinfo = () => {
+  changeSelectedText("#", "#");
+};
+const ref_md_edit: Ref<HTMLElement | null> = ref(null);
+console.log(ref_md_edit);
+
+const changeSelectedText = (startString: string, endString: string) => {
+  let t = ref_md_edit;
+  const selection = window.getSelection();
+  if (selection) {
+    if (
+      t.value.selectionStart != undefined &&
+      t.value.selectionEnd != undefined
+    ) {
+      let str1 = t.value.value.substring(0, t.value.selectionStart);
+      let str2 = t.value.value.substring(
+        t.value.selectionStart,
+        t.value.selectionEnd
+      );
+      let str3 = t.value.value.substring(t.value.selectionEnd);
+
+      let result = str1 + startString + str2 + endString + str3;
+      t.value = result;
+      markString.value = t.value;
+    }
+  }
+};
+
+const svgNames = ref<Array<string>>([
+  "a-edit-office-richtext-headline-style",
+  "ziyuan",
+  "a-edit-office-italic-tilt-richtext",
+  "a-edit-office-richtext-quote",
+  "lianjie",
+  "tupian",
+  "a-edit-office-richtext-program",
+  "daimapianduan",
+  "brackets-curly",
+  "a-edit-office-richtext-list",
+  "youxuliebiao",
+  "a-edit-office-richtext-slash-delete",
+  "zu",
+  "a-edit-office-richtext-table",
+  "a-edit-office-richtext-centeralined",
+]);
 </script>
 
 <style lang="scss" scoped>
@@ -157,6 +222,7 @@ const htmlString = ref("");
   display: flex;
   display: -webkit-flex;
   flex-direction: column;
+  height: 100%;
   .button_bar {
     width: 100%;
     height: 35px;
@@ -168,28 +234,30 @@ const htmlString = ref("");
     align-items: center;
     .button-left {
       display: grid;
-      grid-template-columns: repeat(12, 1fr);
+      grid-template-columns: repeat(15, 50px);
+      align-items: center;
       margin-left: 20px;
       .button-editor {
+        text-align: center;
         cursor: pointer;
+        width: 30px;
+        height: 30px;
         &:hover {
-          width: 100%;
-          height: 80%;
+          border-radius: 5%;
           background-color: rgb(225, 228, 232);
         }
       }
     }
   }
   .content_bar {
-    display: flex;
-    display: -webkit-flex;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    border-bottom: solid 1px #e6e6e6;
     width: 100%;
-    height: 100%;
+    height: 86vh;
     .markdown_body {
-      width: 50%;
+      width: 100%;
       height: 100%;
-      display: flex;
-      display: -webkit-flex;
     }
 
     .html_body {
@@ -201,7 +269,7 @@ const htmlString = ref("");
     }
 
     .md_textarea_content {
-      flex: 1;
+      width: 100%;
       height: 100%;
       padding: 12px;
       overflow: auto;
@@ -209,11 +277,27 @@ const htmlString = ref("");
       resize: none;
       outline: none;
       border: none;
-      background-color: #ffffff;
+      border-right: solid 1px #e6e6e6;
+      background-color: #fafbfc;
       font-size: 14px;
       color: #232323;
       line-height: 24px;
     }
+  }
+}
+.bottom {
+  font-size: 12px;
+  line-height: 24px;
+  border-top: 1px solid #e1e4e8;
+  border-top: transparent;
+  .bytemd-status-left {
+    float: left;
+  }
+  .bytemd-status-right {
+    float: right;
+  }
+  span {
+    padding-left: 16px;
   }
 }
 </style>

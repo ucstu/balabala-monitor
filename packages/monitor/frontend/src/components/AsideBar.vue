36a1ff38bdb3fa2fc45d4c51e301bccc44f7cfe7
qiyuanhong <3201295685@qq.com>
2022-08-04 10:59:32 +0800

    监控系统侧边栏和顶部栏基础样式完成

--- packages/monitor/frontend/src/components/AsideBar.vue
+++ packages/monitor/frontend/src/components/AsideBar.vue
@@ -1,5 +1,13 @@
 <template>
-  <aside style="background-color: red"></aside>
+  <div class="leftmenu">
+    <ul>
+      <li><i class="fa fa-desktop"></i><a href="#">总览</a></li>
+      <li><i class="fa fa-desktop"></i><a href="#">总览</a></li>
+      <li><i class="fa fa-desktop"></i><a href="#">总览</a></li>
+      <li><i class="fa fa-desktop"></i><a href="#">总览</a></li>
+      <li><i class="fa fa-desktop"></i><a href="#">总览</a></li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -10,4 +18,29 @@ const props = defineProps<{
 }>();
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.leftmenu {
+  width: 220px;
+  height: 100%;
+  background-color: rgba(196, 193, 193, 0.6);
+  padding: 20px 0;
+}
+.leftmenu > ul > li {
+  height: 42px;
+  line-height: 42px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
+}
+.leftmenu > ul > li:hover {
+  background-color: rgb(190, 184, 184);
+}
+.leftmenu > ul {
+  margin-top: 5px;
+}
+.leftmenu > ul > li > a {
+  text-decoration: none;
+  color: rgb(100, 98, 98);
+  margin-left: 30px;
+}
+</style>
--- packages/monitor/frontend/src/components/HeaderBar.vue
+++ packages/monitor/frontend/src/components/HeaderBar.vue
@@ -1,5 +1,23 @@
 <template>
-  <header style="background-color: blue"></header>
+  <div class="main">
+    <div class="left">
+      <i class="fa fa-bars"></i>
+      <span
+        ><i class="fa fa-cloud-upload"></i> <strong>balabala云</strong></span
+      >
+    </div>
+    <div class="right">
+      <span><i class="fa fa-search"></i></span>
+      <span><input type="text" /></span>
+      <a href="#" class="i">费用</a>
+      <a href="#" class="i">工单</a>
+      <a href="#" class="i">ICP备案</a>
+      <a href="#" class="i">企业</a>
+      <a href="#" class="i">支持</a>
+      <a href="#" class="fa fa-envelope-o i"></a>
+      <img src="@/assets/no_user.png" alt="#" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -10,4 +28,69 @@ const props = defineProps<{
 }>();
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main {
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  width: 100%;
+  background-color: rgba(213, 211, 211, 0.5);
+}
+.left {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.left span {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.left .fa-bars {
+  font-size: 25px;
+  padding: 12.5px 14px;
+  color: white;
+  background-color: rgb(63, 63, 84);
+}
+.left .fa-cloud-upload {
+  font-size: 30px;
+}
+.left .fa-cloud-upload,
+.left strong {
+  color: rgb(63, 63, 84);
+  padding: 0 10px;
+}
+.right {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5px 10px;
+}
+.right span input {
+  border: none;
+  background-color: rgba(212, 210, 210, 0.5);
+}
+.right span input:focus {
+  outline: none;
+}
+.right span {
+  background-color: rgba(212, 210, 210, 0.5);
+  padding: 5px 5px;
+  display: flex;
+  align-items: center;
+}
+.right .i {
+  padding: 5px 20px;
+}
+.right a {
+  text-decoration: none;
+  color: grey;
+}
+.right a:hover {
+  color: black;
+  transition: top 0.3s ease;
+}
+.right img {
+  margin: 0 30px 0 20px;
+}
+</style>

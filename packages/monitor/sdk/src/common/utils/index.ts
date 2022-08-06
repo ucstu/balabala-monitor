f7a06d5df35fdc41b1420e7e63781761206135bd
zxd2219 <2219454275@qq.com>
2022-08-06 05:22:11 +0000

    修复了点问题

--- packages/monitor/sdk/src/common/utils/index.ts
+++ packages/monitor/sdk/src/common/utils/index.ts
@@ -1,27 +1,5 @@
 import { getConfig } from "../config";
 
-export let cacheDataBase: IDBDatabase;
-export const initReportCache = (): Promise<void> => {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open("monitorReportCache", 1);
-    request.onerror = function (_event) {
-      reject();
-    };
-    request.onsuccess = function (_event) {
-      cacheDataBase = request.result;
-      resolve();
-    };
-    request.onupgradeneeded = function (event) {
-      cacheDataBase = (event.target as EventTarget & { result: IDBDatabase })
-        .result;
-      if (!cacheDataBase.objectStoreNames.contains("cache")) {
-        cacheDataBase.createObjectStore("cache", { autoIncrement: true });
-      }
-      resolve();
-    };
-  });
-};
-
 export const getAppId = (): string => {
   return getConfig().appId;
 };
@@ -49,3 +27,5 @@ export const getBasicParams = () => {
     userID: getUserID(),
   };
 };
+
+export * from "./lifeCycle";
--- packages/monitor/sdk/src/index.ts
+++ packages/monitor/sdk/src/index.ts
@@ -1,15 +1,8 @@
 import { MonitorConfig, setConfig, SetOptional } from "./common/config";
-import { initReportCache } from "./common/utils";
 
 import startMonitoring from "./monitoring";
 
 export const createMonitor = (config: SetOptional<MonitorConfig, "userId">) => {
   setConfig(config);
-  initReportCache()
-    .then(() => {
-      startMonitoring();
-    })
-    .catch(() => {
-      console.log("初始化失败");
-    });
+  startMonitoring();
 };

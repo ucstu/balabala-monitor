d87ce28b7d2006ba7d0808d9b0a6bc44647f0cd5
zxd2219 <2219454275@qq.com>
2022-08-12 12:14:02 +0000

    完整路由配置

--- packages/monitor/frontend/src/App.vue
+++ packages/monitor/frontend/src/App.vue
@@ -23,9 +23,19 @@
 <script lang="ts" setup>
 import AsideBar from "@/components/AsideBar.vue";
 import HeaderBar from "@/components/HeaderBar.vue";
+import { watch } from "vue";
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+
+watch(
+  () => route.meta.title,
+  () => {
+    document.title = route.meta.title
+      ? "前端监控-" + (route.meta.title || "")
+      : "Balabala云-前端监控";
+  }
+);
 </script>
 
 <style scoped lang="scss"></style>

e300d0e305e4ef69b540e8879c2522220408aac2
zxd2219 <2219454275@qq.com>
2022-08-13 00:04:56 +0000

    修复掘金前端页面结构

--- packages/juejin/frontend/src/pages/HomePage/HomePage.vue
+++ packages/juejin/frontend/src/pages/HomePage/HomePage.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <HeaderBar />
     <SideBar />
   </div>
 </template>
 
 <script setup lang="ts">
-import HeaderBar from "@/components/HeaderBar.vue";
 import SideBar from "../../components/SideBar.vue";
 </script>

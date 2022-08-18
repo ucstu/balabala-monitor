f975cfd2e3b3c53267a9126f193fde8867c4a3d3
董健 <1174115923@qq.com>
2022-08-18 22:41:20 +0800

    开发中

--- packages/monitor/frontend/src/pages/systemSet/healthStatus.vue
+++ packages/monitor/frontend/src/pages/systemSet/healthStatus.vue
@@ -1,21 +1,41 @@
 <template>
   <div class="bg">
     <header class="header">
-      <div class="title">健康状况</div>
+      <div class="title">健康数据</div>
       <div class="calendar">
-        <input type="text" placeholder="今天" /><i class="fa fa-calendar-o"></i>
+        <input v-model="ErrorListParms.starttime" type="date" />
       </div>
     </header>
     <!-- 指标 -->
+    <div class="center">
+      <!-- 健康状态 -->
+      <div></div>
 
-    <div class=""></div>
+      <!-- js错误 -->
+      <div></div>
+
+      <!-- 静态资源异常 -->
+      <div></div>
+
+      <!-- 接口异常 -->
+      <div></div>
+    </div>
 
     <!-- 图像 -->
-    <div class=""></div>
+    <div class="bottom"></div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import dayjs from "dayjs";
+
+const ErrorListParms = $ref({
+  appid: "b2FdF9cb-1EE7-Dc6e-de9C-1cAcf37dcdd5",
+  //userid: "ce124A07-9111-C9b7-84C5-5644CEdB4Abf",
+  starttime: dayjs().format("YYYY-MM-DD"),
+  endtime: dayjs().add(1, "day").format("YYYY-MM-DD"),
+});
+</script>
 
 <style lang="scss" scoped>
 .bg {

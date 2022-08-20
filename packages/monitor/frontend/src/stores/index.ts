fbc5e0318bda87d0a64f864d37b6ca67b011862c
金小莉 <3194447668@qq.com>
2022-08-20 22:11:24 +0800

    不知道干了啥

--- packages/monitor/frontend/src/stores/index.ts
+++ packages/monitor/frontend/src/stores/index.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export const useStore = defineStore("main", () => {
-  const appId = $ref("");
+  const appId = ref("b2FdF9cb-1EE7-Dc6e-de9C-1cAcf37dcdd5");
   return { appId };
 });
---
+++ packages/monitor/frontend/src/types/index.ts
@@ -0,0 +1,7 @@
+export interface BasicStatistic {
+  dateTime: string;
+  count: number;
+  average: number;
+  userCount: number;
+  pageCount: number;
+}

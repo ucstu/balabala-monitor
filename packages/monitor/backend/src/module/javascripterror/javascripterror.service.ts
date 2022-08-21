561ad96d684546b803d1ea704c7dc976a32047cc
张腾飞 <2936408477@qq.com>
2022-08-21 12:41:51 +0800

    refactor: 重写js错误列表查询接口

--- packages/monitor/backend/src/module/javascripterror/javascripterror.service.ts
+++ packages/monitor/backend/src/module/javascripterror/javascripterror.service.ts
@@ -4,15 +4,20 @@ import * as dayjs from "dayjs";
 import { javascripterrorIndex } from "src/config/db.index";
 import { JavaScriptError } from "src/entity/javaScriptError.entity";
 import { responseRust } from "src/entity/responseRust";
-import { getQueryBody, getTotalErrorBody } from "src/utils/searchBody";
+import { getTotalErrorBody } from "src/utils/searchBody";
 import {
   JavaScriptErrorTotalVo,
   JavaScriptErrorVo,
 } from "src/vo/javascripterror.vo";
-
+const SqlString = require("sqlstring");
 @Injectable()
 export class JavascripterrorService {
   constructor(private readonly elasticsearchService: ElasticsearchService) {}
+  /**
+   * js 错误,上报数据
+   * @param javaScriptError
+   * @returns
+   */
   async uploadError(javaScriptError: JavaScriptError[]) {
     const body = [];
     javaScriptError.forEach((e) => {
@@ -29,29 +34,83 @@ export class JavascripterrorService {
     return responseRust.error("上传失败,原因:" + JSON.stringify(res));
   }
 
+  /**
+   * js 错误列表查询
+   * @param querys
+   * @returns
+   */
   async getErrorList(querys: JavaScriptErrorVo) {
-    const body = getQueryBody(querys, "errorTime");
-    const res = await this.elasticsearchService.search({
-      index: javascripterrorIndex,
-      body,
+    //const body = getQueryBody(querys, "errorTime");
+    if (querys.start_time.length === 10) {
+      querys.start_time = querys.start_time + " 00:00:00";
+      querys.end_time = querys.end_time + " 00:00:00";
+    }
+    let sqlString = `
+            SELECT msg,count(msg),url ,line,column,userID,pageUrl
+            FROM "javascript_error"
+            where appId=? and mainType=? and mainType=? and errorTime between ? and ?
+            group by msg,url, line,column,userID,pageUrl
+            order by count(msg) desc
+        `;
+    const sqlAges = [
+      querys.app_id,
+      querys.main_type,
+      querys.sub_type,
+      new Date(querys.start_time).getTime(),
+      new Date(querys.end_time).getTime(),
+    ];
+    if (querys.size) {
+      sqlString += " limit ?";
+      sqlAges.push(querys.size);
+    }
+    const sql = SqlString.format(sqlString, sqlAges);
+    const rest = await this.elasticsearchService.sql.query({
+      body: {
+        query: sql,
+      },
     });
-    if (res.statusCode !== 200) {
+    if (rest.statusCode !== 200) {
       return responseRust.error();
     }
-    const rest = {
-      items: [],
-      totalCount: 0,
-    };
-    const list: JavaScriptError[] = [];
-    res.body.hits.hits.forEach((element) => {
-      const source: JavaScriptError = element._source;
-      list.push(source);
+    const map = new Map();
+    rest.body.rows.forEach((item) => {
+      const value = {
+        msg: item[0],
+        count: item[1],
+        url: item[2],
+        line: item[3],
+        column: item[4],
+        userCount: 1,
+        pageCount: 1,
+        userID: [item[5]],
+        pageUrl: [item[6]],
+        average: 0,
+      };
+      const key = `${item[0]}${item[2]}${item[3]}${item[4]}`;
+      if (map.has(key)) {
+        const mapItem = map.get(key);
+        if (!mapItem.userID.includes(value.userID[0])) {
+          mapItem.userCount++;
+          mapItem.userID.push(value.userID[0]);
+        }
+        if (!mapItem.pageUrl.includes(value.pageUrl[0])) {
+          mapItem.pageCount++;
+          mapItem.pageUrl.push(value.pageUrl[0]);
+        }
+        map.get(key).count += value.count;
+      } else {
+        map.set(key, value);
+      }
     });
-    rest.items = list;
-    rest.totalCount = res.body.hits.total.value;
-    return responseRust.success_data(rest);
+
+    return responseRust.success_data([...map.values()]);
   }
 
+  /**
+   * js错误,统计查询
+   * @param querys
+   * @returns
+   */
   async totalError(querys: JavaScriptErrorTotalVo) {
     const body = getTotalErrorBody(querys);
     const res = await this.elasticsearchService.search({

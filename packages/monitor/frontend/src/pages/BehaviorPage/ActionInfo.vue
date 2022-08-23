7dbd074493cfe4b634e3ecf4d6375d37637a850c
zxd2219 <2219454275@qq.com>
2022-08-23 07:13:38 +0000

    重构卡片和behavior

--- packages/monitor/frontend/src/pages/BehaviorPage/ActionInfo.vue
+++ packages/monitor/frontend/src/pages/BehaviorPage/ActionInfo.vue
@@ -1,206 +1,206 @@
 <template>
-  <div v-if="chosesAction !== -1">
+  <div v-if="actionType !== -1">
     <div class="action-info-item">
       <div class="action-info-item-title">事件类型：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.title }}
+        {{ actionInfo.title }}
       </div>
     </div>
     <div class="action-info-item">
       <div class="action-info-item-title">发生时间：</div>
       <div class="action-info-item-data">
-        {{ dayjs(showActionInfo.time).format("YYYY-MM-DD HH:mm:ss") }}
+        {{ dayjs(actionInfo.time).format("YYYY-MM-DD HH:mm:ss") }}
       </div>
     </div>
     <div class="action-info-item">
       <div class="action-info-item-title">发生页面：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.pageUrl }}
+        {{ actionInfo.pageUrl }}
       </div>
     </div>
     <!-- 资源错误 -->
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 1"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 1"
       class="action-info-item"
     >
       <div class="action-info-item-title">资源类型：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.resourceType }}
+        {{ actionInfo.resourceType }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 1"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 1"
       class="action-info-item"
     >
       <div class="action-info-item-title">资源路径：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.path }}
+        {{ actionInfo.path }}
       </div>
     </div>
     <!-- js错误 -->
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">文件路径：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.url }}
+        {{ actionInfo.url }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">错误消息：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.msg }}
+        {{ actionInfo.msg }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">错误行号：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.line }}
+        {{ actionInfo.line }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">错误列号：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.column }}
+        {{ actionInfo.column }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">错误调用堆栈：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.stack }}
+        {{ actionInfo.stack }}
       </div>
     </div>
     <!-- Promise 错误-->
     <div
-      v-if="showActionInfo.listType === 2 && showActionInfo.mainType === 3"
+      v-if="actionInfo.listType === 2 && actionInfo.mainType === 3"
       class="action-info-item"
     >
       <div class="action-info-item-title">错误调用堆栈：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.stack }}
+        {{ actionInfo.stack }}
       </div>
     </div>
     <!-- 点击行为 -->
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">点击位置距离页面左端距离：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.left }}
+        {{ actionInfo.left }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">点击位置距离页面顶端距离：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.top }}
+        {{ actionInfo.top }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">点击对象：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.target }}
+        {{ actionInfo.target }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 2"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 2"
       class="action-info-item"
     >
       <div class="action-info-item-title">内部文本：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.inner }}
+        {{ actionInfo.inner }}
       </div>
     </div>
     <!-- 页面跳转行为 -->
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 3"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 3"
       class="action-info-item"
     >
       <div class="action-info-item-title">路由跳转起步位置：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.from }}
+        {{ actionInfo.from }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 3"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 3"
       class="action-info-item"
     >
       <div class="action-info-item-title">路由跳转目的位置：</div>
-      <div class="action-info-item-data">{{ showActionInfo.to }}</div>
+      <div class="action-info-item-data">{{ actionInfo.to }}</div>
     </div>
     <!-- 路由跳转 -->
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 4"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 4"
       class="action-info-item"
     >
       <div class="action-info-item-title">路径参数：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.params }}
+        {{ actionInfo.params }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 1 && showActionInfo.mainType === 4"
+      v-if="actionInfo.listType === 1 && actionInfo.mainType === 4"
       class="action-info-item"
     >
       <div class="action-info-item-title">查询参数：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.query }}
+        {{ actionInfo.query }}
       </div>
     </div>
     <!-- 接口 -->
     <div
-      v-if="showActionInfo.listType === 3 && showActionInfo.mainType === 5"
+      v-if="actionInfo.listType === 3 && actionInfo.mainType === 5"
       class="action-info-item"
     >
       <div class="action-info-item-title">接口地址：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.url }}
+        {{ actionInfo.url }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 3 && showActionInfo.mainType === 5"
+      v-if="actionInfo.listType === 3 && actionInfo.mainType === 5"
       class="action-info-item"
     >
       <div class="action-info-item-title">状态码：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.statusCode }}
+        {{ actionInfo.statusCode }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 3 && showActionInfo.mainType === 5"
+      v-if="actionInfo.listType === 3 && actionInfo.mainType === 5"
       class="action-info-item"
     >
       <div class="action-info-item-title">请求方式：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.method }}
+        {{ actionInfo.method }}
       </div>
     </div>
     <div
-      v-if="showActionInfo.listType === 3 && showActionInfo.mainType === 5"
+      v-if="actionInfo.listType === 3 && actionInfo.mainType === 5"
       class="action-info-item"
     >
       <div class="action-info-item-title">接口耗时：</div>
       <div class="action-info-item-data">
-        {{ showActionInfo.duration }}
+        {{ actionInfo.duration }}
       </div>
     </div>
   </div>
@@ -208,14 +208,12 @@
 
 <script setup lang="ts">
 import dayjs from "dayjs";
-import { Info } from "./types";
+import type { ActionInfo } from "./types";
 
-type Props = {
-  chosesAction: number;
-  showActionInfo: Info;
-};
-
-defineProps<Props>();
+const { actionType, actionInfo } = defineProps<{
+  actionType: number;
+  actionInfo: ActionInfo;
+}>();
 </script>
 
 <style lang="scss" scoped>

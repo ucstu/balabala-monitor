0dca4126d14cfbca39bb1350a36e30691043a088
张旭东 <2219454275@qq.com>
2022-08-24 16:25:51 +0800

    .

--- packages/juejin/frontend/src/pages/DetailPage/DetailPage.vue
+++ packages/juejin/frontend/src/pages/DetailPage/DetailPage.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="whole">
     <div style="flex: 1"></div>
-    <div class="main-content"></div>
+    <div class="main-content">
+      <MarkdownViewer :mark-string="article.articleContent" />
+    </div>
     <div class="right-content"></div>
     <div style="flex: 1"></div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Article, getArticle } from "@/apis";
+import { useRoute } from "vue-router";
+import MarkdownViewer from "./MarkdownViewer.vue";
+
+const route = useRoute();
+let article = $ref<Article>();
+
+getArticle({
+  articleId:
+    typeof route.params.id === "string" ? route.params.id : route.params.id[0],
+}).then((res) => {
+  article = res.data;
+});
+</script>
 <style>
 .whole {
   display: flex;

d4dda3fe8d273f56a2780c9852afba98b791be72
qiyuanhong <3201295685@qq.com>
2022-08-24 11:36:26 +0800

    概览unknow报错解决，健康界面初步完成

--- packages/monitor/frontend/src/pages/OverviewPage/HealthStatusOverview.vue
+++ packages/monitor/frontend/src/pages/OverviewPage/HealthStatusOverview.vue
@@ -10,16 +10,31 @@
       title="一天的用户访问量趋势"
       icon="fa-dedent"
       line="title"
-      style="display: flex; justify-content: space-between; margin-top: 20px"
-    >
-      <ECharts
-        :option="total_options"
-        :autoresize="true"
-        class="bar1"
-      ></ECharts>
-      <div>
-        <ECharts></ECharts>
-        <ECharts></ECharts>
+      style="margin-top: 20px"
+      ><div
+        style="
+          display: flex;
+          justify-content: space-between;
+          text-align: center;
+        "
+      >
+        <ECharts
+          :option="total_options"
+          :autoresize="true"
+          class="bar1"
+        ></ECharts>
+        <div style="display: flex; align-items: center; width: 650px">
+          <ECharts
+            :option="interface_options"
+            :autoresize="true"
+            class="bar2"
+          ></ECharts>
+          <ECharts
+            :option="resourse_options"
+            :autoresize="true"
+            class="bar2"
+          ></ECharts>
+        </div>
       </div>
     </DataCard>
   </div>
@@ -33,37 +48,211 @@ import { EChartsCoreOption } from "echarts";
 import DataCard from "@/components/DataCard.vue";
 import { useStore } from "@/stores";
 import { basicChartOption } from "@/configs";
+import {
+  getErrorsInterfaceerrorstatistics,
+  getErrorsResourceerrorstatistics,
+  getPerformancesInterfaceindicatorstatistics,
+  getPerformancesResourceindicatorstatistics,
+  InterfaceIndicator,
+  ResourceIndicator,
+} from "@/apis";
+const store = useStore();
 let date = $ref<string>(dayjs().format("YYYY-MM-DD"));
-let total_options = $ref<EChartsCoreOption>({
-  title: {
-    zlevel: 2, // 控制圆环图中间的字的层级
-    text: "58%",
-    top: "47%", // 控制位置
-    left: "47%", // 控制位置
-    textAlign: "center", // 让文字居中
-    textStyle: {
-      color: "yellow",
-      fontWeight: "bolder",
-      fontSize: "30px",
-    },
-  },
-  series: [
-    {
-      name: "Access From",
-      type: "pie",
-      radius: ["40%", "70%"],
-      label: {
-        position: "inside",
+let interfaceStatistics = $ref<number>(0);
+let resourseStatistics = $ref<number>(0);
+let interfaceErrorStatistics_code400 = $ref<number>(0);
+// let interfaceErrorStatistics_code500 = $ref<number>(0);
+let resourseErrorStatistics = $ref<number>(0);
+let total_options = $computed<EChartsCoreOption>(() => {
+  return {
+    title: {
+      zlevel: 2, // 控制圆环图中间的字的层级
+      text:
+        (
+          (1 -
+            (resourseErrorStatistics + interfaceErrorStatistics_code400) /
+              (interfaceStatistics +
+                resourseStatistics +
+                resourseErrorStatistics)) *
+          100
+        ).toFixed(2) + "%",
+      top: "45%",
+      left: "50%",
+      textAlign: "center", // 让文字居中
+      textStyle: {
+        color: "red",
+        fontWeight: "bolder",
+        fontSize: "30px",
       },
-      data: [
-        { value: 0, name: "请求总数" },
-        { value: 0, name: "错误总数" },
-      ],
     },
-  ],
-  ...basicChartOption,
+    series: [
+      {
+        name: "Access From",
+        type: "pie",
+        radius: ["40%", "70%"],
+        label: {
+          position: "inside",
+        },
+        data: [
+          {
+            value:
+              interfaceStatistics +
+              resourseStatistics +
+              resourseErrorStatistics,
+            name: "请求总数",
+          },
+          {
+            value: resourseErrorStatistics + interfaceErrorStatistics_code400,
+            name: "错误总数",
+          },
+        ],
+      },
+    ],
+    ...basicChartOption,
+  };
 });
-// onMounted(()=>{})
+let interface_options = $computed<EChartsCoreOption>(() => {
+  return {
+    title: {
+      zlevel: 2, // 控制圆环图中间的字的层级
+      text:
+        (
+          (interfaceErrorStatistics_code400 / interfaceStatistics) *
+          100
+        ).toFixed(2) + "%",
+      top: "45%",
+      left: "50%",
+      textAlign: "center", // 让文字居中
+      textStyle: {
+        color: "red",
+        fontWeight: "bolder",
+        fontSize: "30px",
+      },
+    },
+    series: [
+      {
+        name: "interface",
+        type: "pie",
+        radius: ["40%", "70%"],
+        label: {
+          position: "inside",
+        },
+        data: [
+          {
+            value: interfaceStatistics,
+            name: "接口请求",
+          },
+          {
+            value: interfaceErrorStatistics_code400,
+            name: "接口错误",
+          },
+        ],
+      },
+    ],
+    ...basicChartOption,
+  };
+});
+let resourse_options = $computed<EChartsCoreOption>(() => {
+  return {
+    title: {
+      zlevel: 2, // 控制圆环图中间的字的层级
+      text:
+        (
+          (resourseErrorStatistics /
+            (resourseStatistics + resourseErrorStatistics)) *
+          100
+        ).toFixed(2) + "%",
+      top: "45%",
+      left: "50%",
+      textAlign: "center", // 让文字居中
+      textStyle: {
+        color: "red",
+        fontWeight: "bolder",
+        fontSize: "30px",
+      },
+    },
+    series: [
+      {
+        name: "resourse",
+        type: "pie",
+        radius: ["40%", "70%"],
+        label: {
+          position: "inside",
+        },
+        data: [
+          {
+            value: resourseStatistics + resourseErrorStatistics,
+            name: "资源请求",
+          },
+          {
+            value: resourseErrorStatistics,
+            name: "资源错误",
+          },
+        ],
+      },
+    ],
+    ...basicChartOption,
+  };
+});
+onMounted(() => {
+  getalldata(date);
+});
+watch(
+  () => date,
+  (newValue) => {
+    getalldata(newValue);
+  }
+);
+function getalldata(start: string) {
+  getPerformancesInterfaceindicatorstatistics({
+    appId: store.appId,
+    startTime: start,
+    endTime: dayjs(start).add(1, "day").format("YYYY-MM-DD"),
+    mainType: InterfaceIndicator.mainType.InterfaceIndicator,
+    subType: InterfaceIndicator.subType.InterfaceIndicator,
+    granularity: "1d",
+  }).then((res) => {
+    let total = 0;
+    res.data.forEach((e: any) => {
+      total += e[0].count;
+    });
+    interfaceStatistics = total;
+    console.log(total);
+  });
+  getErrorsInterfaceerrorstatistics({
+    appId: store.appId,
+    startTime: start,
+    endTime: dayjs(start).add(1, "day").format("YYYY-MM-DD"),
+    mainType: InterfaceIndicator.mainType.InterfaceIndicator,
+    subType: InterfaceIndicator.subType.InterfaceIndicator,
+    granularity: "1d",
+  }).then((res) => {
+    interfaceErrorStatistics_code400 = res.data[0].count;
+    console.log(res.data[0].count);
+  });
+  getPerformancesResourceindicatorstatistics({
+    appId: store.appId,
+    startTime: start,
+    endTime: dayjs(start).add(1, "day").format("YYYY-MM-DD"),
+    mainType: ResourceIndicator.mainType.ResourceIndicator,
+    subType: ResourceIndicator.subType.ResourceIndicator,
+    granularity: "1d",
+  }).then((res) => {
+    resourseStatistics = res.data[0].count;
+    console.log(res.data[0].count);
+  });
+  getErrorsResourceerrorstatistics({
+    appId: store.appId,
+    startTime: start,
+    endTime: dayjs(start).add(1, "day").format("YYYY-MM-DD"),
+    mainType: ResourceIndicator.mainType.ResourceIndicator,
+    subType: ResourceIndicator.subType.ResourceIndicator,
+    granularity: "1d",
+  }).then((res) => {
+    resourseErrorStatistics = res.data[0].count;
+    console.log(res.data[0].count);
+  });
+}
 </script>
 
 <style lang="scss" scoped>
@@ -95,8 +284,15 @@ let total_options = $ref<EChartsCoreOption>({
   }
 
   .bar1 {
+    display: flex;
     width: 300px;
     height: 300px;
+    text-align: center;
+  }
+
+  .bar2 {
+    width: 250px;
+    height: 250px;
   }
 }
 </style>

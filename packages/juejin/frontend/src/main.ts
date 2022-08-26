400937e9bc588d18011c8542ee0faf0755cb3059
张旭东 <2219454275@qq.com>
2022-08-26 15:50:41 +0000

    模拟用户ID

--- packages/juejin/frontend/src/main.ts
+++ packages/juejin/frontend/src/main.ts
@@ -8,12 +8,37 @@ import App from "./App.vue";
 import router from "./router";
 import "./style.scss";
 
+function guid() {
+  function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
+}
+
 import("@balabala/monitor-sdk")
   .then(({ createMonitor }) => {
+    let userId = localStorage.getItem("userId");
+    if (!userId) {
+      userId = guid();
+      localStorage.setItem("userId", userId);
+    }
     createMonitor({
       url: import.meta.env.VITE_MONITOR_URL,
       appId: "b2FdF9cb-1EE7-Dc6e-de9C-1cAcf37dcdd5",
-      userId: "ce124A07-9111-C9b7-84C5-5644CEdB4Abf",
+      userId: userId as string,
     });
   })
   .catch((err) => {
